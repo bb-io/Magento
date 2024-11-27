f277a546bf4b671af24364d259274e6eb1462a1f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-11-27 13:23:33 +0200

    Added Category IDs optional input for product action and event

--- Apps.Magento/Models/Requests/Products/FilterProductRequest.cs
+++ Apps.Magento/Models/Requests/Products/FilterProductRequest.cs
@@ -1,4 +1,6 @@
+using Apps.Magento.DataSources;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Magento.Models.Requests.Products;
 
@@ -6,4 +8,7 @@ public class FilterProductRequest : BaseFilterRequest
 {    
     [Display("Name", Description = "You can filter by name")]
     public override string? Title { get; set; }
+    
+    [Display("Category IDs", Description = "Filter products that belong to all specified category IDs"), DataSource(typeof(CategoryDataHandler))]
+    public IEnumerable<string>? CategoryIds { get; set; }
 }
